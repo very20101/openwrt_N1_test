123e075f3e43d3999bcad05657488fbb7f2bab1c
welvery2010 <very20101@users.noreply.github.com>
2022-12-22 08:09:42 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -119,7 +119,7 @@ sed -i '/bin\/sh/a\uci set nlbwmon.@nlbwmon[0].refresh_interval=2s' package/lean
 sed -i '/nlbwmon/a\uci commit nlbwmon' package/lean/default-settings/files/zzz-default-settings
 
 # 修改版本为编译日期
-date_version=$(date +"%Y.%m.%d")
+date_version=$(date +"%y.%m.%d")
 orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
 sed -i "s/${orig_version}/R${date_version} by Haiibo/g" package/lean/default-settings/files/zzz-default-settings
 
