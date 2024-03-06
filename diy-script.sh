65e6d7dbb1e11f3749147387ea02df4ed47ad638
welvery2010 <very20101@users.noreply.github.com>
2024-03-06 10:03:13 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -131,5 +131,9 @@ find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/
 # sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/model/cbi/v2ray_server/*.lua
 # sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/view/v2ray_server/*.htm
 
+# golang replace
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
