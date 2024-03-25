b7ace12e28deb2301ededf204fc3568123915135
welvery2010 <very20101@users.noreply.github.com>
2024-03-25 14:08:02 +0800

    Update diy-script-new.sh

--- diy-script-new.sh
+++ diy-script-new.sh
@@ -154,8 +154,8 @@ rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 
 # libxslt
-rm -rf feeds/packages/lib/libxslt
-merge_package https://github.com/openwrt/packages/libs/libxslt feeds/packages/lib/libxslt
+#rm -rf feeds/packages/lib/libxslt
+#merge_package https://github.com/openwrt/packages/libs/libxslt feeds/packages/lib/libxslt
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
