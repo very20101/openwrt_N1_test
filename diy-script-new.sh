42dc2134aaa5fc87a8c2474843c235d118ef011e
welvery2010 <very20101@users.noreply.github.com>
2024-06-20 04:43:51 +0800

    Update diy-script-new.sh

--- diy-script-new.sh
+++ diy-script-new.sh
@@ -157,33 +157,17 @@ git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/l
 #rm -rf feeds/packages/lib/libxslt
 #merge_package https://github.com/openwrt/packages/libs/libxslt feeds/packages/lib/libxslt
 
-# perl
-rm rf package/openwrt_N1_test
-rm -rf feeds/packages/lang/perl
+# extra package
+rm -rf feeds/packages/lang/perl 
+rm -ef feeds/packages/lang/python/python-bidict package/network/services/hostapd 
+rm -rf feeds/packages/lang/python/python-setuptools-scm  feeds/packages/lang/host-pip-requirements/setuptools-scm.txt
+git clone https://github.com/very20101/openwrt_N1_test package/openwrt_N1_test
 
-git clone -b main https://github.com/very20101/openwrt_N1_test package/openwrt_N1_test
 mv package/openwrt_N1_test/perl feeds/packages/lang/perl
-
-# extra package 
-rm -rf feeds/packages/lang/perl-xml-parser feeds/packages/lang/python/python-bidict 
-rm -rf package/network/services/hostapd  feeds/packages/lang/python/python-setuptools-scm
-rm -rf feeds/packages/lang/python/python-setuptools-scm feeds/packages/lang/python/python-dateutil
-rm -rf feeds/packages/lang/python/python-installer feeds/packages/lang/python/python-installer
-rm -rf feeds/packages/lang/python/python-build feeds/packages/lang/python/python-packaging
-rm -rffeeds/packages/lang/python/python-typing-extensions
-
-#git clone -b main https://github.com/very20101/openwrt_N1_test package/openwrt_N1_test
-mv package/openwrt_N1_test/extra_pack/hostapd package/network/services/hostapd
-mv package/openwrt_N1_test/perl feeds/packages/lang/perl-xml-parser
 mv package/openwrt_N1_test/extra_pack/python-bidict feeds/packages/lang/python/python-bidict
+mv package/openwrt_N1_test/extra_pack/hostapd package/network/services/hostapd
 mv package/openwrt_N1_test/extra_pack/python-setuptools-scm feeds/packages/lang/python/python-setuptools-scm
-mv package/openwrt_N1_test/extra_pack/python-dateutil feeds/packages/lang/python/python-dateutil
-
-mv package/openwrt_N1_test/extra_pack/python-wheel feeds/packages/lang/python/python-wheel
-mv package/openwrt_N1_test/extra_pack/python-installer feeds/packages/lang/python/python-installer
-mv package/openwrt_N1_test/extra_pack/python-build feeds/packages/lang/python/python-build
-mv package/openwrt_N1_test/extra_pack/python-packaging feeds/packages/lang/python/python-packaging
-mv package/openwrt_N1_test/extra_pack/python-typing-extensions feeds/packages/lang/python/python-typing-extensions
+mv package/openwrt_N1_test/extra_pack/host-pip-requirements/setuptools-scm.txt feeds/packages/lang/host-pip-requirements/setuptools-scm.txt
 
 rm -rf package/openwrt_N1_test
 
