717981cf59748cc8f8443a5af145be660bdb3b17
welvery2010 <very20101@users.noreply.github.com>
2025-03-09 16:26:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -37,19 +37,19 @@ find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_U
 find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
 
 # extra package for perl python
-rm -rf feeds/packages/lang/perl feeds/packages/lang/python/python-bidict package/network/services/hostapd 
-rm -rf feeds/packages/lang/python/python-setuptools-scm  feeds/packages/lang/python/host-pip-requirements/setuptools-scm.txt
-rm -rf feeds/packages/lang/python/python-dateutil
-git clone https://github.com/very20101/openwrt_N1_test package/openwrt_N1_test
+#rm -rf feeds/packages/lang/perl feeds/packages/lang/python/python-bidict package/network/services/hostapd 
+#rm -rf feeds/packages/lang/python/python-setuptools-scm  feeds/packages/lang/python/host-pip-requirements/setuptools-scm.txt
+#rm -rf feeds/packages/lang/python/python-dateutil
+#git clone https://github.com/very20101/openwrt_N1_test package/openwrt_N1_test
 
-mv package/openwrt_N1_test/perl feeds/packages/lang/perl
-mv package/openwrt_N1_test/extra_pack/python-bidict feeds/packages/lang/python/python-bidict
-mv package/openwrt_N1_test/extra_pack/hostapd package/network/services/hostapd
-mv package/openwrt_N1_test/extra_pack/python-setuptools-scm feeds/packages/lang/python/python-setuptools-scm
-mv package/openwrt_N1_test/extra_pack/host-pip-requirements/setuptools-scm.txt feeds/packages/lang/python/host-pip-requirements/setuptools-scm.txt
-mv package/openwrt_N1_test/extra_pack/python-dateutil feeds/packages/lang/python/python-dateutil
+#mv package/openwrt_N1_test/perl feeds/packages/lang/perl
+#mv package/openwrt_N1_test/extra_pack/python-bidict feeds/packages/lang/python/python-bidict
+#mv package/openwrt_N1_test/extra_pack/hostapd package/network/services/hostapd
+#mv package/openwrt_N1_test/extra_pack/python-setuptools-scm feeds/packages/lang/python/python-setuptools-scm
+#mv package/openwrt_N1_test/extra_pack/host-pip-requirements/setuptools-scm.txt feeds/packages/lang/python/host-pip-requirements/setuptools-scm.txt
+#mv package/openwrt_N1_test/extra_pack/python-dateutil feeds/packages/lang/python/python-dateutil
 
-rm -rf package/openwrt_N1_test
+#rm -rf package/openwrt_N1_test
 
 ./scripts/feeds update -a
 ./scripts/feeds install -f
