c416f232768de615f07bad0c36e24cacae3d5d6d
welvery2010 <very20101@users.noreply.github.com>
2025-06-25 00:45:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,9 +17,9 @@ sed -i 's/192.168.1.1/192.168.1.200/g' package/base-files/files/bin/config_gener
 # sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
 # extra package
-#git clone https://github.com/kenzok8/small-package package/small-package
-git clone https://github.com/kenzok8/openwrt-packages package/kenzo
-git clone https://github.com/kenzok8/small package/small-deps
+git clone https://github.com/kenzok8/small-package package/small-package
+#git clone https://github.com/kenzok8/openwrt-packages package/kenzo
+#git clone https://github.com/kenzok8/small package/small-deps
 #rm -rf package/small-package/firewall
 
 # 修改本地时间格式
