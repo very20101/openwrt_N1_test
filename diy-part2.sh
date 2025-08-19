67b5bff2013c20c9a5b693f18f19a0b2c285410d
welvery2010 <very20101@users.noreply.github.com>
2025-08-20 00:58:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,7 +10,7 @@
 # extra package from https://github.com/kenzok8
 #===============================================
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.1.200/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.100/g' package/base-files/files/bin/config_generate
 # Change default shell to zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 # TTYD 免登录
