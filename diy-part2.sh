c8db06e611179a73d29da12737aa69252a92bc01
welvery2010 <very20101@users.noreply.github.com>
2026-01-22 14:46:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -64,8 +64,8 @@ cp -rf package/openwrt_N1_test/extra_pack/miniupnpc feeds/packages/net/miniupnpd
 #mv package/openwrt_N1_test/extra_pack/host-pip-requirements/setuptools-scm.txt feeds/packages/lang/python/host-pip-requirements/setuptools-scm.txt
 #mv package/openwrt_N1_test/extra_pack/python-dateutil feeds/packages/lang/python/python-dateutil
 
-#rm -rf tools/cmake
-#cp -rf package/openwrt_N1_test/extra_pack/cmake tools/cmake
+rm -rf tools/cmake
+cp -rf package/openwrt_N1_test/extra_pack/cmake tools/cmake
 
 #rm -rf feeds/packages/lang/golang
 #git clone https://github.com/kenzok8/golang -b 1.25 feeds/packages/lang/golang
